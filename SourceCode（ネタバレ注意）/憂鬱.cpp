d7bd55e80c1fa2eb9fad0881e33712f19307dc5b
さとしん <131766015+satoshin-des@users.noreply.github.com>
2024-02-17 12:49:11 +0900

    Add files via upload

--- SourceCode（ネタバレ注意）/憂鬱.cpp
+++ SourceCode（ネタバレ注意）/憂鬱.cpp
@@ -1,90 +1,56 @@
 ﻿#include <iostream>
 #include<string>
 #include <windows.h>
-#include <conio.h>
-
-void YukiNagatoPrintEndFirst(const std::string str) {
-    Sleep(1000);
-    const int n = str.size();
-    printf("YUKI.N> ");
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-}
-
-void YukiNagatoPrintEnd(const std::string str) {
-    Sleep(1000);
-    const int n = str.size();
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-}
-
-void YukiNagatoPrintFirst(const std::string str) {
-    Sleep(1000);
-    printf("YUKI.N> ");
-    const int n = str.size();
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-    puts("");
-}
 
 void YukiNagatoPrint(const std::string str) {
-    Sleep(1000);
+    //Sleep(1000);
     const int n = str.size();
+    printf("YUKI.N> ");
     for (int i = 0; i < n; ++i) {
         printf("%c", str.at(i));
+        if (i > 2) {
+            if (str.substr(i - 2, std::string("。").length()) == "。") { Sleep(1000); }
+            else if (str.substr(i - 2, std::string("、").length()) == "、") Sleep(500);
+        }
         Sleep(50);
     }
-    puts("");
 }
 
 int main() {
     std::string kyon;
 
-    YukiNagatoPrintFirst("みえてる？\n");
+    YukiNagatoPrint("みえてる？\n\n");
 
     printf("KYON> ");
     std::cin >> kyon; puts("");
 
     if (kyon == "いや" || kyon == "見えない" || kyon == "いや。" || kyon == "見えない。" || kyon == "みえない" || kyon == "みえない。" || kyon == "みえてない" || kyon == "みえてない。" || kyon == "見えてない" || kyon == "見えてない。") {
-        YukiNagatoPrintFirst("今はつまらない冗談をいっている場合ではない。\n");
+        YukiNagatoPrint("今はつまらない冗談をいっている場合ではない。\n\n");
     }
 
-    YukiNagatoPrintEndFirst("そっちの時空間とはまだ完全に連結を絶たれていない。");
-    YukiNagatoPrintEnd("でも時間の問題。");
-    YukiNagatoPrint("そうなれば最後。\n");
+    YukiNagatoPrint("そっちの時空間とはまだ完全に連結を絶たれていない。でも時間の問題。そうなれば最後。\n\n");
 
     printf("KYON> ");
     std::cin >> kyon; puts("");
 
-    YukiNagatoPrintEndFirst("どうにもならない。");
-    YukiNagatoPrintEnd("情報統合思念体は失望している。");
-    YukiNagatoPrint("これで進化の可能性は失われた。\n");
+    YukiNagatoPrint("どうにもならない。情報統合思念体は失望している。これで進化の可能性は失われた。\n\n");
 
-    YukiNagatoPrintEndFirst("涼宮ハルヒは何もない所から情報を生み出す力を持っていたそれは情報統合思念体にもない力。");
-    YukiNagatoPrint("この情報創造能力を解析すれば自律進化の糸口がつかめるかもしれないと考えた\n");
-
-    YukiNagatoPrintFirst("あなたに賭ける\n");
+    YukiNagatoPrint("涼宮ハルヒは何もない所から情報を生み出す力を持っていた。\n");
+    YukiNagatoPrint("それは情報統合思念体にもない力。この情報創造能力を解析すれば自律進化の糸口がつかめるかもしれないと考えた。\n");
+    YukiNagatoPrint("あなたに賭ける。\n\n");
 
     printf("KYON> ");
     std::cin >> kyon; puts("");
 
     if (kyon == "いや" || kyon == "やだ" || kyon == "いや。" || kyon == "やだ。") {
-        YukiNagatoPrintFirst("あなたに拒否権はない。\n");
+        YukiNagatoPrint("あなたに拒否権はない。\n\n");
     }
 
-    YukiNagatoPrintEndFirst("もう一度こちらへ回帰することを我々は望んでる。");
-    YukiNagatoPrintEnd("涼宮ハルヒは重要な監視対象。");
-    YukiNagatoPrint("わたしという固体もあなたには戻ってきて欲しいと感じている\n");
+    YukiNagatoPrint("もう一度こちらへ回帰することを我々は望んでる。涼宮ハルヒは重要な監視対象。わたしという固体もあなたには戻ってきて欲しいと感じている。\n\n");
 
-    YukiNagatoPrintFirst("また図書館に\n");
+    YukiNagatoPrint("また図書館に\n\n");
 
-    YukiNagatoPrintFirst("sleeping deauty");
+    YukiNagatoPrint("sleeping deauty\n\n");
 
     Sleep(1000);
 
