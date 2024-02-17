96e2843609976bbe7f633bb06f52c846eb9a0c57
さとしん <131766015+satoshin-des@users.noreply.github.com>
2024-02-17 12:48:31 +0900

    Add files via upload

--- SourceCode（ネタバレ注意）/消失.cpp
+++ SourceCode（ネタバレ注意）/消失.cpp
@@ -3,73 +3,46 @@
 #include <windows.h>
 #include <conio.h>
 
-void YukiNagatoPrintEndFirst(const std::string str) {
-    Sleep(1000);
-    const int n = str.size();
-    printf("YUKI.N> ");
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-}
-
-void YukiNagatoPrintEnd(const std::string str) {
-    Sleep(1000);
-    const int n = str.size();
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-}
-
-void YukiNagatoPrintFirst(const std::string str) {
-    Sleep(1000);
-    printf("YUKI.N> ");
-    const int n = str.size();
-    for (int i = 0; i < n; ++i) {
-        printf("%c", str.at(i));
-        Sleep(50);
-    }
-    puts("");
-}
-
 void YukiNagatoPrint(const std::string str) {
-    Sleep(1000);
+    //Sleep(1000);
     const int n = str.size();
+    printf("YUKI.N> ");
     for (int i = 0; i < n; ++i) {
         printf("%c", str.at(i));
+        if (i > 2) {
+            if (str.substr(i - 2, std::string("。").length()) == "。") { Sleep(1000); }
+            else if (str.substr(i - 2, std::string("、").length()) == "、") Sleep(500);
+        }
         Sleep(50);
     }
-    puts("");
 }
 
 int main(){
     int c;
+    Sleep(1000);
     
-    YukiNagatoPrintFirst("これをあなたが読んでいる時、わたしはわたしでないだろう。\n");
+    YukiNagatoPrint("これをあなたが読んでいる時、わたしはわたしでないだろう。\n\n");
     
-    YukiNagatoPrintFirst("このメッセージが表示されたということは、そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、古泉一樹が存在しているはずである。\n");
+    YukiNagatoPrint("このメッセージが表示されたということは、そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、古泉一樹が存在しているはずである。\n\n");
     
-    YukiNagatoPrintEndFirst("それが鍵。");
-    YukiNagatoPrint("あなたは解答を見つけ出した。\n");
+    YukiNagatoPrint("それが鍵。あなたは解答を見つけ出した。\n\n");
     
-    YukiNagatoPrintEndFirst("これは緊急脱出プログラムである。");
-    YukiNagatoPrintEnd("起動させる場合はエンターキーを、そうでない場合はそれ以外のキーを選択せよ。");
-    YukiNagatoPrintEnd("起動させた場合、あなたは時空修正の機会を得る。");
-    YukiNagatoPrintEnd("ただし成功は保証できない。");
-    YukiNagatoPrint("また帰還の保証もできない。\n");
+    YukiNagatoPrint("これは緊急脱出プログラムである。起動させる場合はエンターキーを、そうでない場合はそれ以外のキーを選択せよ。起動させた場合、あなたは時空修正の機会を得る。ただし成功は保証できない。また帰還の保証もできない。\n\n");
     
-    YukiNagatoPrintEndFirst("このプログラムが起動するのは一度きりである。");
-    YukiNagatoPrintEnd("実行ののち、消去される。");
-    YukiNagatoPrintEnd("非実行が選択された場合は起動せずに消去される。\n");
-    YukiNagatoPrint("Ready?");
-    
-    while ((c = _getch()) == '\r') return 0;
-    YukiNagatoPrintFirst("その選択をしたのがあなた自身であるならば、異論はない。\n");
-    
-    YukiNagatoPrintFirst("ありがとう。\n");
+    YukiNagatoPrint("このプログラムが起動するのは一度きりである。実行ののち、消去される。非実行が選択された場合は起動せずに消去される。 Ready?");
 
-    YukiNagatoPrintFirst("また、どこかで。\n");
+    while ((c = _getch()) == '\r') { 
+        return 0; 
+    }
+    puts("\n");
+
+    YukiNagatoPrint("その選択をしたのがあなた自身であるならば、異論はない。\n\n");
+    
+    YukiNagatoPrint("ありがとう。\n\n");
+
+    YukiNagatoPrint("また、どこかで。\n\n");
+
+    YukiNagatoPrint("God bless...");
 
     Sleep(1000);
     return 0;
